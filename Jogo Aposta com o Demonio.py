3f2fa9b1282c9128e261dcba29f482cbc08d3032
GusChoairy <101018515+GusChoairy@users.noreply.github.com>
2022-03-26 16:38:24 -0300

    Update Jogo Aposta com o Demonio.py

--- Jogo Aposta com o Demonio.py
+++ Jogo Aposta com o Demonio.py
@@ -7,25 +7,30 @@ dadoD = int(random.randint(1,6))
 dadoD2= int(random.randint(1,6))
 dadoH = int(random.randint(1,6))
 
+cores = {'vermelho': '\033[1;31;40m',
+         'limpa': '\033[m',
+         'Vitoria':'\033[1;33;107m',
+         'empate':'\033[36;107m'}
+
 print('O dado do Demonio: ',(dadoD))
 print('O seu dado:', (dadoH))
 
 #Se os dados do Humano for menor que o do Demonio:
 
 if dadoH < dadoD:
-    print('"Nínguem nunca lhe ensinou a não apostar contra um Demônio!?RMRMRMRMhahaha..."')
+    print(cores['vermelho'],'"Nínguem nunca lhe ensinou a não apostar contra um Demônio!?RMRMRMRMhahaha..."',cores['limpa'])
 
 #se os dados do Humano for igual ao do Demonio:
 
 elif dadoD == dadoH:
-    print('=== EMPATE ===\nAgora terá que advinhar o número que cairá no dado do Demõnio. Você realmente esperava que fosse ser assim tão simples ganhar do próprio MALTAEL!?')
+    print('\033[36;107m=== EMPATE ===\033[m''\nAgora terá que advinhar o número que cairá no dado do Demõnio. Você realmente esperava que fosse ser assim tão simples ganhar do próprio MALTAEL!?')
     advinhação = int(input('Agora Diga! Que numeros cairão nos dados? '))
     resposta = str(input('Tem CERTEZA da sua resposta? ')).strip()
 
 #se a resposta for SIM:
 
     if resposta.lower() == 'sim' or resposta.lower() == 's':
-        print('"Pois bem! rmrmrmm..."')
+        print(cores['vermelho'],'"Pois bem! rmrmrmm..."',cores['limpa'])
         time.sleep(2)
         print('O Demônio lança seus dados e o resultado é surpreendente!')
         print(dadoD, 'e', dadoD2)
@@ -34,15 +39,15 @@ elif dadoD == dadoH:
 #se ganhar com resposta SIM:
 
         if dadoD == advinhação and dadoD2 == advinhação:
-           print('MAS COMO PODE UM VERME COMO VOCÊ GANHAR NUMA APOSTA CONTRA O DEMÔNIO!')
+           print(cores['vermelho'],'"MAS COMO PODE UM VERME COMO VOCÊ GANHAR NUMA APOSTA CONTRA O DEMÔNIO!"', cores['limpa'])
 
 #se perder com resposta SIM:
         else:
-            print('O RESULTADO NÃO PODERIA SER OUTRO! SOMENTE UM ANIMAL PARA SER TÃO IGNORANTE A PONTO DE ACHAR QUE PODERIA GANHAR NUM JOGO DE AZAR CONTRA O SENHOR DAS MARUAGEM!')
+            print(cores['vermelho'],'"O RESULTADO NÃO PODERIA SER OUTRO! SOMENTE UM ANIMAL PARA SER TÃO IGNORANTE A PONTO DE ACHAR QUE PODERIA GANHAR NUM JOGO DE AZAR CONTRA O SENHOR DAS MARUAGEM!"',cores['limpa'])
 
 #se a resposta for NÃO:
     else:
-        print('"QUE SEJA {} ENTÃO!"'.format(advinhação))
+        print(cores['vermelho'],'"QUE SEJA {} ENTÃO!"',cores['limpa'].format(advinhação))
         print('O Demonio lança seus dados e o resultado foi surpreendente!')
         print(dadoD,'e', dadoD2)
         print('O ardiloso Demônio possuia mais um dado.')
@@ -50,12 +55,12 @@ elif dadoD == dadoH:
 #se ganhar com resposta NÃO:
         if dadoD == advinhação and dadoD2 == advinhação:
 #RESPOSTA PRA AMIGOS:
-            print('"MAS COMO PODE UM VERME COMO VOCÊ GANHAR NUMA APOSTA CONTRA O DEMÔNIO!"')
+            print(cores['vermelho'],'"MAS COMO PODE UM VERME COMO VOCÊ GANHAR NUMA APOSTA CONTRA O DEMÔNIO!"',cores['limpa'])
 
 #se perder com resposta NÃO:
         else:
-            print('"O RESULTADO NÃO PODERIA SER OUTRO! SOMENTE UM ANIMAL PARA SER TÃO IGNORANTE A PONTO DE ACHAR QUE PODERIA GANHAR NUM JOGO DE AZAR CONTRA O SENHOR DAS MARUAGEM!"\n"AGORA TODOS QUE CONHECE QUEIMARÃO E SUA ALMA ME SERVIRÁ PARA SEMPRE!"')
+            print(cores['vermelho'],'"O RESULTADO NÃO PODERIA SER OUTRO! SOMENTE UM ANIMAL PARA SER TÃO IGNORANTE A PONTO DE ACHAR QUE PODERIA GANHAR NUM JOGO DE AZAR CONTRA O SENHOR DAS MARUAGEM!"\n"AGORA TODOS QUE CONHECE QUEIMARÃO E SUA ALMA ME SERVIRÁ PARA SEMPRE!"',cores['limpa'])
 
 
 else:
-    print('O DEMONIO FICOU REVOLTADO COM O RESULTADO E VOCÊ MORREU!')
+    print(cores['Vitoria'],'**',cores['empate'],'VOCÊ FOI UM HOMEM HUMILDE E OS DEUSES O ABENÇOARAM!',cores['Vitoria'],'**',cores['limpa'])
